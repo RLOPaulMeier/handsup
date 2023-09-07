8e2115729e72f568b5122bbd225659c2be8952d6
Emanuel <84103687+RLOPaulMeier@users.noreply.github.com>
2023-09-07 23:04:32 +0200

    Add files via upload

---
+++ handsup/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games {'gta5'}
+
+author 'vz_lanarhoades_xv'
+description 'Simple Handsup Script with Command and Key funtion'
+version '1.2'
+
+shared_script "config.lua"
+
+client_scripts { 
+	'client.lua',
+	'config.lua'
+}
